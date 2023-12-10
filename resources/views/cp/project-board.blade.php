0751df2f12ea111ccd4d3f01b3fdcf2a24076879
John Koster <john@stillat.com>
2023-12-10 08:55:19 -0600

    Update project-board.blade.php

--- resources/views/cp/project-board.blade.php
+++ resources/views/cp/project-board.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="flex items-center justify-between mb-3">
-        <h1>{{ __('site.project_board') }}</h1>
+        <h1>{{ __('agency_site.project_board') }}</h1>
     </div>
 
     <project-board :columns="@js($projects)"></project-board>
